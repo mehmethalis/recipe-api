3ae1381fde97bfad4d189be934d43a5a0da86fbb
mehmethalis <mehmethaliscicek@gmail.com>
2022-03-20 20:09:21 +0300

    modified settings.py

--- app/app/settings.py
+++ app/app/settings.py
@@ -81,6 +81,11 @@ DATABASES = {
         'NAME': os.environ.get('DB_NAME'),
         'USER': os.environ.get('DB_USER'),
         'PASSWORD': os.environ.get('DB_PASSWORD'),
+        'TEST': {
+            'NAME': os.environ.get('DB_NAME'),
+            'USER': os.environ.get('DB_USER'),
+            'PASSWORD': os.environ.get('DB_PASSWORD'),
+        }
     }
 }
 
