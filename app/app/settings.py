e9f5db29aa61c79235efc33562f18049ccde1374
mehmethalis <mehmethaliscicek@gmail.com>
2022-04-01 21:13:24 +0300

    added ingredient endpoint

--- app/app/settings.py
+++ app/app/settings.py
@@ -78,11 +78,11 @@ WSGI_APPLICATION = 'app.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'HOST': os.environ.get('DB_HOST'),
-        'NAME': os.environ.get('DB_NAME'),
-        'USER': os.environ.get('DB_USER'),
-        'PASSWORD': os.environ.get('DB_PASS'),
-        'PORT': os.environ.get('DB_PORT')
+        'HOST': 'db',
+        'NAME': 'app',
+        'USER': 'postgres',
+        'PASSWORD': 'secret',
+        'PORT': '5454'
     }
 }
 
